4c89c08f4b25f77b610d2449e37c2b3d5cdb67fb
Tommi Tervonen <tervonen@ese.eur.nl>
2011-11-24 10:50:31 +0100

    Finish ConstraintFactory & its tests

--- src/main/java/fi/smaa/prefsample/ConstraintFactoryTest.java
+++ src/main/java/fi/smaa/prefsample/ConstraintFactoryTest.java
@@ -18,5 +18,42 @@ public class ConstraintFactoryTest {
 		assertEquals(0.0, c.getValue(), 0.000001);
 		assertArrayEquals(new double[]{-1.0, 0.0, 1.0, 0.0, 0.0}, c.getCoefficients().getData(), 0.000001);
 	}
+	
+	@Test
+	public void testCreateUpperRatioConstraint() {
+		LinearConstraint c = ConstraintFactory.createUpperRatioConstraint(5, 0, 2, 1.2);
+	
+		assertEquals(Relationship.LEQ, c.getRelationship());
+		assertEquals(0.0, c.getValue(), 0.000001);
+		assertArrayEquals(new double[]{1.0, 0.0, -1.2, 0.0, 0.0}, c.getCoefficients().getData(), 0.000001);
+	}
+	
+	@Test
+	public void testCreateLowerRatioConstraint() {
+		LinearConstraint c = ConstraintFactory.createLowerRatioConstraint(5, 0, 2, 1.2);
+	
+		assertEquals(Relationship.LEQ, c.getRelationship());
+		assertEquals(0.0, c.getValue(), 0.000001);
+		assertArrayEquals(new double[]{-1.0, 0.0, 1.2, 0.0, 0.0}, c.getCoefficients().getData(), 0.000001);
+	}	
+	
+	
+	@Test
+	public void testCreateUpperBoundConstraint() {
+		LinearConstraint c = ConstraintFactory.createUpperBoundConstraint(5, 0, 0.8);
+		
+		assertEquals(Relationship.LEQ, c.getRelationship());
+		assertEquals(0.8, c.getValue(), 0.000001);
+		assertArrayEquals(new double[]{1.0, 0.0, 0.0, 0.0, 0.0}, c.getCoefficients().getData(), 0.000001);
+	}
+	
+	@Test
+	public void testCreateLowerBoundConstraint() {
+		LinearConstraint c = ConstraintFactory.createLowerBoundConstraint(5, 0, 0.8);
+		
+		assertEquals(Relationship.LEQ, c.getRelationship());
+		assertEquals(-0.8, c.getValue(), 0.000001);
+		assertArrayEquals(new double[]{-1.0, 0.0, 0.0, 0.0, 0.0}, c.getCoefficients().getData(), 0.000001);
+	}	
 
 }
--- src/test/java/fi/smaa/prefsample/ConstraintFactory.java
+++ src/test/java/fi/smaa/prefsample/ConstraintFactory.java
@@ -7,16 +7,95 @@ public class ConstraintFactory {
 
 	/**
 	 * Create a constraint expressing that w1 >= w2
+	 * PRECOND: w1 >= 0 && w1 < dim
+	 * PRECOND: w2 >= 0 && w2 < dim
 	 * @param dim dimension
 	 * @param w1 index of w1
 	 * @param w2 index of w2
 	 * @return
 	 */
 	public static LinearConstraint createOrdinalConstraint(int dim, int w1, int w2) {
+		assert (w1 >= 0 && w1 < dim);
+		assert (w2 >= 0 && w2 < dim);
 		double[] coefficients = new double[dim];
 		coefficients[w1] = -1;
 		coefficients[w2] = 1;
 		LinearConstraint c = new LinearConstraint(coefficients, Relationship.LEQ, 0.0);
 		return c;
 	}
+	
+	/**
+	 * Create a constraint expressing that w1/w2 <= x
+	 * PRECOND: w1 >= 0 && w1 < dim
+	 * PRECOND: w2 >= 0 && w2 < dim
+	 * @param dim dimension
+	 * @param w1 index of w1
+	 * @param w2 index of w2
+	 * @param x the ratio upper bound
+	 * @return
+	 */
+	public static LinearConstraint createUpperRatioConstraint(int dim, int w1, int w2, double x) {
+		assert (w1 >= 0 && w1 < dim);
+		assert (w2 >= 0 && w2 < dim);
+		double[] coefficients = new double[dim];
+		coefficients[w1] = 1;
+		coefficients[w2] = -x;
+		LinearConstraint c = new LinearConstraint(coefficients, Relationship.LEQ, 0.0);
+		return c;
+	}	
+	
+	/**
+	 * Create a constraint expressing that w1/w2 >= x
+	 * PRECOND: w1 >= 0 && w1 < dim
+	 * PRECOND: w2 >= 0 && w2 < dim
+	 * @param dim dimension
+	 * @param w1 index of w1
+	 * @param w2 index of w2
+	 * @param x the ratio lower bound
+	 * @return
+	 */
+	public static LinearConstraint createLowerRatioConstraint(int dim, int w1, int w2, double x) {
+		assert (w1 >= 0 && w1 < dim);
+		assert (w2 >= 0 && w2 < dim);
+		double[] coefficients = new double[dim];
+		coefficients[w1] = -1;
+		coefficients[w2] = x;
+		LinearConstraint c = new LinearConstraint(coefficients, Relationship.LEQ, 0.0);
+		return c;
+	}	
+	
+	/**
+	 * Create a constraint expressing w1 <= bound
+	 * PRECOND: bound <= 1.0 && bound >= 0.0
+	 * @param dim
+	 * @param w1
+	 * @param bound
+	 * @return
+	 */
+	public static LinearConstraint createUpperBoundConstraint(int dim, int w1, double bound) {
+		assert(bound <= 1.0 && bound >= 0.0);
+		
+		double[] coefficients = new double[dim];
+		coefficients[w1] = 1;
+		LinearConstraint c = new LinearConstraint(coefficients, Relationship.LEQ, bound);
+		return c;
+	}
+	
+	/**
+	 * Create a constraint expressing w1 >= bound
+	 * PRECOND: bound <= 1.0 && bound >= 0.0
+	 * @param dim
+	 * @param w1
+	 * @param bound
+	 * @return
+	 */
+	public static LinearConstraint createLowerBoundConstraint(int dim, int w1, double bound) {
+		assert(bound <= 1.0 && bound >= 0.0);
+		
+		double[] coefficients = new double[dim];
+		coefficients[w1] = -1;
+		LinearConstraint c = new LinearConstraint(coefficients, Relationship.LEQ, -bound);
+		return c;
+	}
+	
 }
